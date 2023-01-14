ad182e1a1ba46ed73cf04f5c729dbc320edeea8a
NiceToMeets <puma1800@naver.com>
2023-01-14 13:20:42 +0900

    router 적용 및 todo 페이지 생성

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
 import Home from './Pages/Home/Home';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Todo from './Pages/Todo/Todo';
 
 function App() {
   return (
-    <Home />
+    
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/todo' element={<Todo />} />
+        <Route path='*' element={<Home />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/Pages/Todo/Todo.jsx
@@ -0,0 +1,18 @@
+function Todo () {
+
+    // 헤더, 푸터
+    // 추가, 수정
+
+    return (
+        <>
+            <strong>제목</strong>
+            <input></input>
+            <strong>설명</strong>
+            <input></input>
+            <strong>반복</strong>
+            <input></input>
+        </>
+    )
+}
+
+export default Todo;
