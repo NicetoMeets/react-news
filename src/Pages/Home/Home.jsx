e9a988b21da45bd79a0e5ac07193f7205a0d41fd
NiceToMeets <puma1800@naver.com>
2023-01-14 12:46:36 +0900

    홈페이지 작성

---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,13 @@
+import News from "../News/News";
+
+function Home () {
+    return (
+        <>
+            <p>I'm Home</p>
+            <News />
+        </>
+        
+    )
+}
+
+export default Home;
